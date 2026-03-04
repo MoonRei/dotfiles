027aafc298c55d0ce629fd5704b17494c9163140
Dan Pehush <65364403+MoonRei@users.noreply.github.com>
2026-03-03 16:54:48 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-DOTFILES_DIR="$HOME/dotfiles"
+set -e
 
-echo "${DOTFILES_DIR}"
 
-ln -sf "$DOTFILES_DIR/vimrc" "$HOME/.vimrc"
-ln -sf "$DOTFILES_DIR/bashrc" "$HOME/.bashrc"
+ln -sf {`pwd`/,~/.}bashrc
+ln -sf {`pwd`/,~/.}vimrc
+
+# Set up the Qumulo toolchain and repos.
+if [ ! -e ~/tools ]; then
+    # apt runs during coder setup and conflicts with toolchain bootstrapping.
+    sleep 60
+    curl -s https://gravyweb.eng.qumulo.com/build/latest/src/build/toolchain/bootstrap.sh | bash
+    ssh-keyscan -4 hg.eng.qumulo.com submit.eng.qumulo.com >> ~/.ssh/known_hosts
+    ssh-keyscan -4 hg submit >> ~/.ssh/known_hosts
+    hg clone --stream --config extensions.qumulo=! ssh://hg@hg.eng.qumulo.com/tools
+    hg clone --stream --config extensions.qumulo=! ssh://hg@hg.eng.qumulo.com/src
+
+    mv tools ~/tools
+    mv src ~/src
+fi
+
+~/src/prebuild
 
 echo "Dotfiles installed."
